6f0410f20de93c90c2b6ad297cffa62f1a2a9d03
Buddamylk <pathwayz2abundance@gmail.com>
2026-06-24 16:39:49 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ejbm-benin.github.io',
-  base: '/',
+  site: 'https://buddamylk.github.io',
+  base: '/EJBM/',
   integrations: [tailwind()],
   output: 'static'
 });
