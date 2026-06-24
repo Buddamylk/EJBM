fe32182902e15084e56af7dfb410eb745df909ce
Buddamylk <pathwayz2abundance@gmail.com>
2026-06-24 18:34:43 -0400

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://buddamylk.github.io',
-  base: '/EJBM/',
-  integrations: [tailwind()],
-  output: 'static'
-});
